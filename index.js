bad53118e8e68411cc5d10d931e92ef6c3079094
Ines Part <ines.part91@gmail.com>
2021-05-06 14:32:09 +0200

    Change fs for node:fs and include comments

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const fs = require('fs');
+// import libraries and declare variables
+const fs = require('node:fs');
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const websiteUrl = 'https://memegen-link-examples-upleveled.netlify.app/';
 const urlList = [];
 
 const getMemeHtml = async () => {
-  // get html text from websiteUrl
+  // get html text from websiteUrl and wait for fetch, then continue
   const response = await fetch(websiteUrl);
   // using await to ensure that the promise resolves
   const body = await response.text();
@@ -15,25 +16,30 @@ const getMemeHtml = async () => {
 
   // using CSS selector
   $('img').each((i, url) => {
-    // console.log(url.attribs.src);
     if (i < 10) {
       urlList.push(url.attribs.src);
     }
   });
 
-  console.log(urlList);
+  // console.log(urlList);
 
-  console.log('urlList', urlList);
+  // create loop that counts to 10
   for (const urlElement of urlList) {
-    console.log('hello', urlElement);
     async function download() {
+      // fetch image data from the list of 10 URLs
       const imageResponse = await fetch(urlElement);
-      // console.log('hello', urlElement);
       const buffer = await imageResponse.buffer();
+      // create files with .jpg extensions and store them in the memes folder
       fs.writeFile(
         `./memes/meme${urlList.indexOf(urlElement)}.jpg`,
         buffer,
-        () => console.log('Download Finished'),
+        () =>
+          // control message after each iteration
+          console.log(
+            `Download of meme${urlList.indexOf(
+              urlElement,
+            )}.jpg was successful.`,
+          ),
       );
     }
     download();
